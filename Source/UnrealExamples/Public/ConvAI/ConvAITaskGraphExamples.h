df48cd37d145c3b65dda4f2650a00e039b4bc9d2
insooneelife <insooneelife@naver.com>
2025-04-20 00:29:16 +0900

    updated delegates on events

--- Source/UnrealExamples/Public/ConvAI/ConvAITaskGraphExamples.h
+++ Source/UnrealExamples/Public/ConvAI/ConvAITaskGraphExamples.h
@@ -85,9 +85,45 @@ public:
 	FString LlmMessage;
 };
 
+// Delegate declarations
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnBeginDelegate, TSharedPtr<FGlobalContext>);
+DECLARE_MULTICAST_DELEGATE(FOnEndDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnPreCollectAudioInputDelegate);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostCollectAudioInputDelegate, const AudioInputTask_Result&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreSttApiDelegate, const TArray<uint8>&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostSttApiDelegate, const SttApiTask_Result&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreLlmApiDelegate, const FString&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostLlmApiDelegate, const LlmApiTask_Result&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreTtsApiDelegate, const FString&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostTtsApiDelegate, const TtsApiTask_Result&);
+DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnPrePlayGameSoundDelegate, const TArray<uint8>&, const UWorld* const, TSharedPtr<FGlobalContext>);
+DECLARE_MULTICAST_DELEGATE_FourParams(FOnPostPlayGameSoundDelegate, const TArray<uint8>&, const UWorld* const, const USoundWaveProcedural* const, TSharedPtr<FGlobalContext>);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreSttDeviceDelegate, const TArray<uint8>&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostSttDeviceDelegate, const SttDeviceTask_Result&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreLlmDeviceDelegate, const FString&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostLlmDeviceDelegate, const LlmDeviceTask_Result&);
+DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnPreUpdateGameByLlmDelegate, const FString&, const UWorld* const, TSharedPtr<FGlobalContext>);
+DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnPostUpdateGameByLlmDelegate, const LlmDeviceTask_Result&, const UWorld* const, TSharedPtr<FGlobalContext>);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPreTtsDeviceDelegate, const FString&);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPostTtsDeviceDelegate, const TtsDeviceTask_Result&);
+
 class FApiFlowContext : public TSharedFromThis<FApiFlowContext, ESPMode::ThreadSafe>
 {
 public:
+	// Delegate instances
+	FOnBeginDelegate OnBeginDelegate;
+	FOnEndDelegate OnEndDelegate;
+	FOnPreCollectAudioInputDelegate OnPreCollectAudioInputDelegate;
+	FOnPostCollectAudioInputDelegate OnPostCollectAudioInputDelegate;
+	FOnPreSttApiDelegate OnPreSttApiDelegate;
+	FOnPostSttApiDelegate OnPostSttApiDelegate;
+	FOnPreLlmApiDelegate OnPreLlmApiDelegate;
+	FOnPostLlmApiDelegate OnPostLlmApiDelegate;
+	FOnPreTtsApiDelegate OnPreTtsApiDelegate;
+	FOnPostTtsApiDelegate OnPostTtsApiDelegate;
+	FOnPrePlayGameSoundDelegate OnPrePlayGameSoundDelegate;
+	FOnPostPlayGameSoundDelegate OnPostPlayGameSoundDelegate;
+
 	void Reset()
 	{
 		Audio_Result.Reset();
@@ -96,31 +132,34 @@ public:
 		Tts_Result.Reset();
 	}
 
-public:
-
 	void OnBegin(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnBegin]"));
+		OnBeginDelegate.Broadcast(GlobalContext);
 	}
 
 	void OnEnd(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnEnd]"));
+		OnEndDelegate.Broadcast();
 	}
 
 	void OnPreCollectAudioInput()
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPreCollectAudioInput]"));
+		OnPreCollectAudioInputDelegate.Broadcast();
 	}
 
 	void OnPostCollectAudioInput(const AudioInputTask_Result& InResult)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPostCollectAudioInput]"));
+		OnPostCollectAudioInputDelegate.Broadcast(InResult);
 	}
 	
 	void OnPreSttApi(const TArray<uint8>& InAudioBuffer)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPreSttApi]"));
+		OnPreSttApiDelegate.Broadcast(InAudioBuffer);
 	}
 
 	void OnPostSttApi(const SttApiTask_Result& InResult)
@@ -132,11 +171,13 @@ public:
 			FString LogMsg = FString::Printf(TEXT("[FApiFlowContext::OnPostSttApi] Stt Response : %s"), *InResult.Message);
 			FConvAIModule::LogWithThreadInfo(LogMsg);
 		}
+		OnPostSttApiDelegate.Broadcast(InResult);
 	}
 
 	void OnPreLlmApi(const FString& InPrompt)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPreLlmApi]"));
+		OnPreLlmApiDelegate.Broadcast(InPrompt);
 	}
 
 	void OnPostLlmApi(const LlmApiTask_Result& InResult)
@@ -148,11 +189,13 @@ public:
 			FString LogMsg = FString::Printf(TEXT("[FApiFlowContext::OnPostLlmApi] Llm Response. : %s"), *InResult.Message);
 			FConvAIModule::LogWithThreadInfo(LogMsg);
 		}
+		OnPostLlmApiDelegate.Broadcast(InResult);
 	}
 
 	void OnPreTtsApi(const FString& InMessage)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPreTtsApi]"));
+		OnPreTtsApiDelegate.Broadcast(InMessage);
 	}
 
 	void OnPostTtsApi(const TtsApiTask_Result& InResult)
@@ -164,12 +207,14 @@ public:
 			FString LogMsg = FString::Printf(TEXT("[FApiFlowContext::OnPostTtsApi] Tts Response : %d"), InResult.AudioBuffer.Num());
 			FConvAIModule::LogWithThreadInfo(LogMsg);
 		}
+		OnPostTtsApiDelegate.Broadcast(InResult);
 	}
 
 	// needs to check if world is valid
 	void OnPrePlayGameSound(const TArray<uint8>& InAudioBuffer, const UWorld* const InWorld, TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPrePlayGameSound]"));
+		OnPrePlayGameSoundDelegate.Broadcast(InAudioBuffer, InWorld, GlobalContext);
 	}
 
 	void OnPostPlayGameSound(
@@ -178,16 +223,12 @@ public:
 		const USoundWaveProcedural* const InSoundWave, 
 		TSharedPtr<FGlobalContext> GlobalContext)
 	{
-		if (!IsValid(InWorld))
-		{
-			return;
-		}
-
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPostPlayGameSound]"));
 		if (IsValid(InSoundWave))
 		{
 			FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPostPlayGameSound] Sound Wave Successfully Created."));
 		}
+		OnPostPlayGameSoundDelegate.Broadcast(InAudioBuffer, InWorld, InSoundWave, GlobalContext);
 	}
 
 
@@ -202,6 +243,18 @@ public:
 class FDeviceFlowContext : public TSharedFromThis<FDeviceFlowContext, ESPMode::ThreadSafe>
 {
 public:
+	// Delegate instances
+	FOnBeginDelegate OnBeginDelegate;
+	FOnEndDelegate OnEndDelegate;
+	FOnPreCollectAudioInputDelegate OnPreCollectAudioInputDelegate;
+	FOnPostCollectAudioInputDelegate OnPostCollectAudioInputDelegate;
+	FOnPreSttDeviceDelegate OnPreSttDeviceDelegate;
+	FOnPostSttDeviceDelegate OnPostSttDeviceDelegate;
+	FOnPreLlmDeviceDelegate OnPreLlmDeviceDelegate;
+	FOnPostLlmDeviceDelegate OnPostLlmDeviceDelegate;
+	FOnPreUpdateGameByLlmDelegate OnPreUpdateGameByLlmDelegate;
+	FOnPostUpdateGameByLlmDelegate OnPostUpdateGameByLlmDelegate;
+
 	void Reset()
 	{
 		Audio_Result.Reset();
@@ -212,52 +265,62 @@ public:
 	void OnBegin(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnBegin]"));
+		OnBeginDelegate.Broadcast(GlobalContext);
 	}
 
 	void OnEnd(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnEnd]"));
+		OnEndDelegate.Broadcast();
 	}
 
 	void OnPreCollectAudioInput()
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPreCollectAudioInput]"));
+		OnPreCollectAudioInputDelegate.Broadcast();
 	}
 
 	void OnPostCollectAudioInput(const AudioInputTask_Result& InResult)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FApiFlowContext::OnPostCollectAudioInput]"));
+		OnPostCollectAudioInputDelegate.Broadcast(InResult);
 	}
 
 
 	void OnPreSttDevice(const TArray<uint8>& InAudioBuffer)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPreSttDevice]"));
+		OnPreSttDeviceDelegate.Broadcast(InAudioBuffer);
 	}
 
 	void OnPostSttDevice(const SttDeviceTask_Result& InResult)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPostSttDevice]"));
+		OnPostSttDeviceDelegate.Broadcast(InResult);
 	}
 
 	void OnPreLlmDevice(const FString& InPrompt)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPreLlmDevice]"));
+		OnPreLlmDeviceDelegate.Broadcast(InPrompt);
 	}
 
 	void OnPostLlmDevice(const LlmDeviceTask_Result& InResult)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPostLlmDevice]"));
+		OnPostLlmDeviceDelegate.Broadcast(InResult);
 	}
 
 	void OnPreUpdateGameByLlm(const FString& InMessage, const UWorld* const InWorld, TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPreUpdateGameByLlm]"));
+		OnPreUpdateGameByLlmDelegate.Broadcast(InMessage, InWorld, GlobalContext);
 	}
 
 	void OnPostUpdateGameByLlm(const LlmDeviceTask_Result& InResult, const UWorld* const InWorld, TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDeviceFlowContext::OnPostUpdateGameByLlm]"));
+		OnPostUpdateGameByLlmDelegate.Broadcast(InResult, InWorld, GlobalContext);
 	}
 
 public:
@@ -269,6 +332,14 @@ public:
 class FDevicePlaySoundFlowContext : public TSharedFromThis<FDevicePlaySoundFlowContext, ESPMode::ThreadSafe>
 {
 public:
+	// Delegate instances
+	FOnBeginDelegate OnBeginDelegate;
+	FOnEndDelegate OnEndDelegate;
+	FOnPreTtsDeviceDelegate OnPreTtsDeviceDelegate;
+	FOnPostTtsDeviceDelegate OnPostTtsDeviceDelegate;
+	FOnPrePlayGameSoundDelegate OnPrePlayGameSoundDelegate;
+	FOnPostPlayGameSoundDelegate OnPostPlayGameSoundDelegate;
+
 	void Reset()
 	{
 		LlmMessage.Reset();
@@ -278,16 +349,19 @@ public:
 	void OnBegin(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnBegin]"));
+		OnBeginDelegate.Broadcast(GlobalContext);
 	}
 
 	void OnEnd(TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnEnd]"));
+		OnEndDelegate.Broadcast();
 	}
 
 	void OnPreTtsDevice(const FString& InMessage)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnPreTtsDevice]"));
+		OnPreTtsDeviceDelegate.Broadcast(InMessage);
 	}
 
 	void OnPostTtsDevice(const TtsDeviceTask_Result& InResult)
@@ -299,11 +373,13 @@ public:
 			FString LogMsg = FString::Printf(TEXT("[FDevicePlaySoundFlowContext::OnPostTtsDevice] Tts Response : %d"), InResult.AudioBuffer.Num());
 			FConvAIModule::LogWithThreadInfo(LogMsg);
 		}
+		OnPostTtsDeviceDelegate.Broadcast(InResult);
 	}
 
 	void OnPrePlayGameSound(const TArray<uint8>& InAudioBuffer, const UWorld* const InWorld, TSharedPtr<FGlobalContext> GlobalContext)
 	{
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnPrePlayGameSound]"));
+		OnPrePlayGameSoundDelegate.Broadcast(InAudioBuffer, InWorld, GlobalContext);
 	}
 
 	void OnPostPlayGameSound(
@@ -312,16 +388,12 @@ public:
 		const USoundWaveProcedural* const InSoundWave,
 		TSharedPtr<FGlobalContext> GlobalContext)
 	{
-		if (!IsValid(InWorld))
-		{
-			return;
-		}
-
 		FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnPostPlayGameSound]"));
 		if (IsValid(InSoundWave))
 		{
 			FConvAIModule::LogWithThreadInfo(TEXT("[FDevicePlaySoundFlowContext::OnPostPlayGameSound] Sound Wave Successfully Created."));
 		}
+		OnPostPlayGameSoundDelegate.Broadcast(InAudioBuffer, InWorld, InSoundWave, GlobalContext);
 	}
 public:
 	FString LlmMessage;
